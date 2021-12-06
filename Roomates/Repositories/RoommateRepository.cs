a4e641d90d7734bd10a228917a3ed94452b19336
Devery Page <pagedevery@gmail.com>
2021-12-06 15:41:09 -0600

    first commit

---
+++ Roomates/Repositories/RoommateRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Data.SqlClient;
+using Roommates.Models;
+
+namespace Roommates.Repositories
+{
+    class RoommateRepository
+    {
+        public SqlConnection Connection { get; private set; }
+
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT FirstName FROM Roommate WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Roommate roommate = null;
+
+                        // If we only expect a single row back from the database, we don't need a while loop.
+                        if (reader.Read())
+                        {
+                            roommate = new Roommate
+                            {
+                                Id = id,
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                            };
+                        }
+                        return roommate;
+                    }
+
+                }
+            }
+        }
+    }
+}
